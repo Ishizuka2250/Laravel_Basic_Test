b3d27e75d9b24fb20edaac1f0c00cbe4a0671313
Ishizuka2250 <squid1028@gmail.com>
2021-09-12 01:38:02 +0900

    First Commit.

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\http\Controllers\TestController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::prefix('building')->group(function() {
+    Route::get('/', [TestController::class, 'building']);
+    Route::get('/{room}', [TestController::class, 'room']);
+});
